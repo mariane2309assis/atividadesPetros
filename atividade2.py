9bbc5ac7299cb1ae44be378c82a747118616cc95
alunofg <mariane.assis.bezerra@gmail.com>
2026-03-04 21:06:02 -0300

    commit 04/03 teste3

--- atividade2.py
+++ atividade2.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import pytesseract
 import matplotlib.pyplot as plt
 
 print(f"OpenCV versão: {cv2.__version__}")
 print(f"NumPy versão: {np.__version__}")
+print(f"Tesseract: {pytesseract.get_tesseract_version()}")
